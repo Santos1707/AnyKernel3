e2494fc4573754ccc2780116c34528ddeaa010da
scorpionsola <79877353+scorpionsola@users.noreply.github.com>
2026-06-19 10:01:35 -0300

    Update kernel string attribution in anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=by belowzeroiq @ github
+kernel.string=by dextermorgan @ github
 do.devicecheck=1
 do.modules=0
 do.systemless=0
